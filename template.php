a872baf2b7b3f8ec72da6bfeb770c00679e332b3
JulciaBurova <yuliiaburova@gmail.com>
2020-06-26 22:31:08 +0200

    master

---
+++ template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv = 'X-UA-Compatible' content ='IE'=edge >
+    <title>My test page</title>
+    <meta name = 'viewport' content = 'widht = device-widht, '>
+  </head>
+  <body>
+    <div>Tekst</div>
+  </body>
+</html>
